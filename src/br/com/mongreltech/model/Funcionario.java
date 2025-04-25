17939c820acf3827abe39e6937ea3fe882b9e888
adrianodomingues <adrianodomingues@hotmail.com>
2025-04-25 13:16:49 -0300

    Projeto de estudo OO Java

--- src/br/com/mongreltech/model/Funcionario.java
+++ src/br/com/mongreltech/model/Funcionario.java
@@ -9,23 +9,17 @@ public class Funcionario
 {
     // instance variables - replace the example below with your own
     private String nome;
-    private Servico servico;
+    private IServico servico;
 
     /**
      * Constructor for objects of class Funcionario
      */
-    public Funcionario(Servico servico)
+    public Funcionario(IServico servico)
     {
         // initialise instance variables
         this.servico = servico;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
     public void aplicarBanhoAnimal(Animal animalAgendado)
     {
         // put your code here
--- src/br/com/mongreltech/model/Gerente.java
+++ src/br/com/mongreltech/model/Gerente.java
@@ -9,23 +9,17 @@ public class Gerente
 {
     // instance variables - replace the example below with your own
     private String nome;
-    private Servico servico;
+    private IServico servico;
 
     /**
      * Constructor for objects of class Gerente
      */
-    public Gerente(Servico servico)
+    public Gerente(IServico servico)
     {
         // initialise instance variables
         this.servico = servico;
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
     public void aplicarTosa(Animal animalAgendado)
     {
         // put your code here
